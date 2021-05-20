484188a48edd6559a232dad7aace16fd1fb6c7ce
Marsel <marsel.kuba67@gmail.com>
2021-05-20 18:31:18 +0600

    4 задание

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ let d = 2;
 let e = "белых медведей"
 
 console.log(c + (d + e));
+
+// 4 задание
+
+let f = 5;  
+let j = 10; 
+let h ;  // h = ++f + j--; надо -- подставить перед перменной j
+console.log(h = ++f + --j)
